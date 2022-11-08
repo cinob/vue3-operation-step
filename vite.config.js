fcb43cfc5f89164ee8a6c1942c615117866651a6
cinob <cinob@foxmail.com>
2022-11-08 18:41:16 +0800

    type: add types

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,8 @@ export default defineConfig({
     }),
 
     Unocss(),
+    
+    dts({ insertTypesEntry: true, copyDtsFiles: false })
   ],
   build: {
     lib: {
